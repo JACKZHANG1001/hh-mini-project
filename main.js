ce871a00173fb937bd0392a3c679a966cf3bfaea
Baozhe <119010421@link.cuhk.edu.cn>
2021-01-25 19:19:47 +0800

    add main

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// import isPrime module
+var isPrime = require('src/isPrime');
+
+var temp = 1;
+
+for (; temp < 100; temp++) {
+    if (isPrime(temp)) {
+        console.log(temp);
+    }
+}
